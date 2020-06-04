90cbf869a3f437f60da10deef5fb516d760834cf
Anton Fedchenko <kompotik@gmail.com>
2020-06-04 17:02:25 +0300

    upd

---
+++ fintrack/precache-manifest.38c5587cbf44263c49a12a723f284455.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4500227aed925e078efce774c3a85611",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81864feb9b7e553df04",
+    "url": "/static/css/main.e380369f.chunk.css"
+  },
+  {
+    "revision": "fe9cf9fb10b9376d2a78",
+    "url": "/static/js/2.4d657ef1.chunk.js"
+  },
+  {
+    "revision": "775071e7908c30b629946039840806b0",
+    "url": "/static/js/2.4d657ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e81864feb9b7e553df04",
+    "url": "/static/js/main.6e277eac.chunk.js"
+  },
+  {
+    "revision": "a85f4c01f8fa8a294825",
+    "url": "/static/js/runtime-main.63c52bf6.js"
+  }
+]);
